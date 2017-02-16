cfff1ed799a398806e21bb755dfef83d68b3e8b6
Mostafa hassan <mostafa.hassan91@hotmail.com>
2017-02-16 05:43:37 +0200

    Create my_ccie_routers.py

---
+++ my_ccie_routers.py
@@ -0,0 +1,42 @@
+P1 = {
+    'device_type': 'cisco_ios',
+    'ip': '1.1.1.1',
+    'username': 'ccie',
+    'password': 'ccie',
+    'port': 22
+}
+P2 = {
+    'device_type': 'cisco_ios',
+    'ip': '2.2.2.2',
+    'username': 'ccie',
+    'password': 'ccie',
+    'port': 22
+}
+P3 = {
+    'device_type': 'cisco_ios',
+    'ip': '3.3.3.3',
+    'username': 'ccie',
+    'password': 'ccie',
+    'port': 22
+}
+P4 = {
+    'device_type': 'cisco_ios',
+    'ip': '4.4.4.4',
+    'username': 'ccie',
+    'password': 'ccie',
+    'port': 22
+}
+PE1 = {
+    'device_type': 'cisco_ios',
+    'ip': '11.11.11.11',
+    'username': 'ccie',
+    'password': 'ccie',
+    'port': 22
+}
+PE2 = {
+    'device_type': 'cisco_ios',
+    'ip': '22.22.22.22',
+    'username': 'ccie',
+    'password': 'ccie',
+    'port': 22
+}
